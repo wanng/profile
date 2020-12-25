f22a4e8221ef06b62d7bfe4368f8e008ee575f90
wangyue <wangyue@wangyuedeMac-mini.local>
2020-12-25 23:02:49 +0800

    update

--- do1/do1.js
+++ do1/do1.js
@@ -110,7 +110,7 @@ var Cookie = {
         var oldCookie = Store.get(cookieKey)
       
         if (!oldCookie || oldCookie != cookie) {
-          console.log("抓取cookie url:" + $request.url)
+          console.log("获取cookie:" + $request.url)
           var success = Store.put(cookieKey, cookie)
           $notify(success ? successTitle : failTitle, "", "")
           $done()
@@ -124,7 +124,7 @@ var Cookie = {
         if (!cookie) {
             return cookie
         }
-        return cookie.split(";").filter(s => s.indexOf(name) != -1)[0]
+        return cookie.split(";").map(s => s.trim()).filter(s => s.indexOf(name) != -1)[0]
     }
 }
 
@@ -177,7 +177,7 @@ var Store = {
     },
 
     put: function (key, value) {
-        console.log("put [" + key + ":" +value+"]")
+        console.log("持久化:[" + key + ":" + value + "]")
         return $prefs.setValueForKey(value, key)
     }
 }
